c072f0f01e51db3f30789f193235e99987ae1852
Temerity-Media <tim@temeritymail.com>
2024-11-05 20:37:19 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/chastitycagesizing/', // Add this line with your repository name as the base
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
